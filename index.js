4bf9a4926c66503729e3502bb225b653b1c4e8ce
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-25 18:12:09 -0400

    commit tree 927be48989d4033812c28f83c8a747774165dde7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+
+
+
+
+
+
+
+
+
+// document.querySelector("main#main").remove();
+
+// let newHeader = document.createElement('h1');
+// newHeader.id = "victory";
+// newHeader.innerHTML = "Hanna is the champion!";
+
